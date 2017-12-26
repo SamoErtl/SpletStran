169b54eb807770f11cae0fb3e08bae38afe2c1a3
SamoErtl <33451251+SamoErtl@users.noreply.github.com>
2017-12-26 12:48:00 +0100

    Add files via upload

--- TPO/View/td-list.php
+++ TPO/View/td-list.php
@@ -9,26 +9,28 @@
 <?php include("view/menu-links.php"); ?>
 
 <p></p>
-<h1 id="center" >All public games</h1>
+<h1 id="center" >All games</h1>
 <table id="td-table">
         <tbody>
     <?php 
     if(!empty($games)){
         foreach ($games as $game):
-            $score=$game['score'];
             if(!is_numeric($game['score']) or 
             !is_numeric($game['time']) or !is_numeric($game['date'])){
                 continue;
             }
-            $time= date("H:i:s", ((int)$game['time']/1000));;
-            $date= date("d-m-Y H:i:s", ((int)$game['date']/1000));;
+            $score=$game['score'];
+            $time= date("H:i:s", ((float)$game['time']/1000));
+            $time= (float)$game['time'];
+            $date= date("d-m-Y H:i:s", ((float)$game['date']/1000));
+            //$date= (float)$game['date']/1000;
             $device=$game['device'];
     ?>      
         
         <tr id="pointsField">
-        <td style="" class="first"><?= $time ?></td>
-        <td class="first"><?= $date ?></td>
-        <td class="first"><?= $score ?></td>
+        <td style="" ><?= $time ?></td>
+        <td ><?= $date ?></td>
+        <td ><?= $score ?></td>
         <td><?= $device ?></td>
         </tr>
         
